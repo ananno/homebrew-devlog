e5a7e7017a0947572dcf0957404c8dc852dd4065
GitLab CI <gitlab-ci@noreply.gitlab.com>
2026-04-14 15:22:34 +0000

    Update devlog to v2.0.3

--- Formula/devlog.rb
+++ Formula/devlog.rb
@@ -2,9 +2,10 @@
 
 class Devlog < Formula
   desc "AI-powered development log generator for git repositories"
-  homepage "https://gitlab.com/aice/devlog"
-  url "https://gitlab.com/aice/devlog/-/archive/v2.0.1/devlog-v2.0.1.tar.gz"
-  sha256 "cda95d301c5d33849a5561635be3a8383bfc904d40c9ec1549dafae9e37396f0"
+  homepage "https://gitlab.com/ananno/devlog"
+  url "https://gitlab.com/ananno/devlog/-/archive/v2.0.3/devlog-v2.0.3.tar.gz"
+  sha256 "c31207d668982d20433eb404fb40ab90d84291436460ab53247ff20c61b38f15"
+  version "2.0.3"
   license "MIT"
 
   depends_on "rust" => :build
@@ -17,6 +18,6 @@ class Devlog < Formula
   end
 
   test do
-    assert_equal version.to_s, `#{bin}/devlog --version`.chomp.split.last
+    assert_match version.to_s, shell_output("#{bin}/devlog --version")
   end
 end
