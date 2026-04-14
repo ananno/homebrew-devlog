a4a0e265c8d23f2a12ecc2a8ada315fe8c427df9
Syed Hasibur Rahman <ananno@pm.me>
2026-04-14 23:14:03 +0900

    updating release

--- Formula/devlog.rb
+++ Formula/devlog.rb
@@ -3,8 +3,8 @@
 class Devlog < Formula
   desc "AI-powered development log generator for git repositories"
   homepage "https://gitlab.com/aice/devlog"
-  url "https://gitlab.com/aice/devlog/-/archive/v1.1.2/devlog-v1.1.2.tar.gz"
-  sha256 "5e9e86429673fc91d05a553da8a4795dc2e6acb25b48f974974918f816162847"
+  url "https://gitlab.com/aice/devlog/-/archive/v2.0.1/devlog-v2.0.1.tar.gz"
+  sha256 "cda95d301c5d33849a5561635be3a8383bfc904d40c9ec1549dafae9e37396f0"
   license "MIT"
 
   depends_on "rust" => :build
